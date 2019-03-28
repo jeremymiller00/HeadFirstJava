0e636ec174bc209f98c2e3847255c1756274cc03
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-28 11:48:01 -0700

    fix null pointer issue

--- BooksTestDrive/BooksTestDrive.java
+++ BooksTestDrive/BooksTestDrive.java
@@ -4,14 +4,22 @@ class BooksTestDrive {
 
         // declare array of Books objects of length 3
         Books[] myBooks = new Books[3]; 
-
+        // counter
         int x = 0;
+
+        // create book objects
+        Books book1 = new Books();
+        myBooks[0] = book1;
+        Books book2 = new Books();
+        myBooks[1] = book2;
+        Books book3 = new Books();
+        myBooks[2] = book3;
         myBooks[0].title = "The Grapes of Java";
         myBooks[1].title = "The Java Gatsby";
-        myBooks[3].title = "The Java Cookbook";
+        myBooks[2].title = "The Java Cookbook";
         myBooks[0].author = "Katelyn";
         myBooks[1].author = "Binh";
-        myBooks[1].author = "Derek";
+        myBooks[2].author = "Derek";
 
         while (x < 3) {
             System.out.print(myBooks[x].title);
