9c59a60345bd711094c6881e5e43b1789c5b980e
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-28 16:24:25 -0700

    start game

---
+++ BattleCom/SimpleDotComGame.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+class SimpleDotComGame {
+
+    public static void main(String[] args) {
+        int numOfGuesses;
+        SimpleDotCom dotCom = new SimpleDotCom();
+
+        Random rand = new Random();
+        int start = rand.nextInt(5);
+
+        // populate location cells
+        
+
+        while 
+    }
+}
