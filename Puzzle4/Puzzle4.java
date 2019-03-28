9bc3adfbe13f1ce5311b91bb9df41d028f7d5e1e
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-28 13:59:09 -0700

    finish puzzle 4

--- Puzzle4/Puzzle4.java
+++ Puzzle4/Puzzle4.java
@@ -2,21 +2,21 @@ public class Puzzle4 {
     
     public static void main(String [] args) {
         
-        // ??
+        Puzzle4b [] obs =  new Puzzle4b[6];
         int y = 1;
         int x = 0;
         int result = 0;
 
         while (x < 6) {
-            // two lines here
-            //
+            obs[x] = new Puzzle4b();
+            obs[x].ivar = y;
             y = y * 10;
-            //
+            x = x + 1;
         }
         x = 6;
         while (x > 0) {
-            //
-            result = result + ??
+            x = x - 1;
+            result = result + obs[x].doStuff(x);
         }
         System.out.println("result " + result);
     }
--- Puzzle4/Puzzle4b.java
+++ Puzzle4/Puzzle4b.java
@@ -1,3 +1,10 @@
 class Puzzle4b {
-    
-}
+    int ivar;
+    public int doStuff(int factor) {
+        if (ivar > 100) {
+            return ivar * factor;
+        } else {
+            return ivar * (5 - factor);
+        } // close loop
+    } // close method    
+} // close class
