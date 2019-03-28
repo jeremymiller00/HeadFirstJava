d7434824e8e5c1b5429441db6bb04f7b708533f2
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-28 13:19:02 -0700

    finish clock

--- Clock/Clock.java
+++ Clock/Clock.java
@@ -2,4 +2,13 @@
 
 class Clock {
     
+    String time;
+
+    void setTime(String t) {
+        time = t;
+    }
+
+    String getTime() {
+        return time;
+    }
 }
--- Clock/ClockTestDrive.java
+++ Clock/ClockTestDrive.java
@@ -1,3 +1,13 @@
 class ClockTestDrive {
     
+    public static void main(String [] args) {
+        
+        Clock c = new Clock();
+
+        c.setTime("12345");
+
+        String tod = c.getTime();
+
+        System.out.println("time: " + tod);
+    }
 }
