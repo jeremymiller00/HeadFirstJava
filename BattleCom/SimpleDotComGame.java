8662b8ac5175c305ee6acf33eb7d0cae22be0fbc
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-28 16:48:39 -0700

    finish simple game

--- BattleCom/SimpleDotComGame.java
+++ BattleCom/SimpleDotComGame.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class SimpleDotComGame {
 
@@ -8,10 +9,24 @@ class SimpleDotComGame {
 
         Random rand = new Random();
         int start = rand.nextInt(5);
-
+        int count = 0;
+        int[] loc = new int[3];
         // populate location cells
-        
+        while (count < 3) {
+            // add start to list
+            loc[count] = start;
+            // increment start
+            start++;
+            count++;
+        }
+        dotCom.setLocationCells(loc);
 
-        while 
+        Scanner in = new Scanner(System.in);
+        while (dotCom.numOfHits < dotCom.locationCells.length) {
+            System.out.println("enter a number: ");
+            String currentGuess = in.nextLine();
+            dotCom.checkYourself(currentGuess);
+
+        }
     }
 }
