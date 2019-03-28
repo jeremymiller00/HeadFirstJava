72a6c40c59ebe0687a12719e843ea3f685960e91
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-28 14:38:54 -0700

    start simpledotcom and test

---
+++ BattleCom/SimpleDotCom.java
@@ -0,0 +1,24 @@
+class SimpleDotCom {
+    int[] locationCells;
+    int numOfHits;
+
+    String checkYourself(String guess) {
+        // get user guess as a string
+        //convert guess to int
+        // for each location cell
+            // if guess == location cell
+                // increment number of hits
+                // if last location cell
+                    // return kill
+                // else
+                    // return hit
+            // else return miss
+    
+    }
+
+    void setLocationCells(int[] loc) {
+        // 
+    }
+    
+
+}
