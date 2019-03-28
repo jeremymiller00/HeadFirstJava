bcefc35530abbb392a0355cbd5ac3f96782d525a
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-28 11:51:26 -0700

    start hobbits

---
+++ Hobbits/Hobbits.java
@@ -0,0 +1,16 @@
+class Hobbits {
+
+    String name;
+
+    public static void main(String[] args) {
+
+        // create list of 3 hobbits
+        Hobbits[] h = new Hobbits[3];
+
+        // counter
+        int z = 0;
+
+        
+        
+    }
+}
