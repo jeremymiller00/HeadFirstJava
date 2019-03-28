346cef925aee007ad52212603805d949d69073ce
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-28 11:54:33 -0700

    first iteration complete

--- Hobbits/Hobbits.java
+++ Hobbits/Hobbits.java
@@ -10,7 +10,18 @@ class Hobbits {
         // counter
         int z = 0;
 
-        
-        
+        while (z < 4) {
+            z = z + 1;
+            h[z] = new Hobbits();
+            n[z].name = "bilbo";
+            if (z == 1) {
+                h[z].name = "frodo";
+            }
+            if (z == 2) {
+                h[z].name = "sam";
+            }
+            System.out.print(h[z].name + " is a ");
+            System.out.println("good Hobbit name.");
+        }
     }
 }
