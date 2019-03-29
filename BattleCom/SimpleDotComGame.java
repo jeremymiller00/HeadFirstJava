0b1192e733ef7420dd241284bc43cc4b841c962e
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-29 10:01:06 -0700

    use book logic

--- BattleCom/SimpleDotComGame.java
+++ BattleCom/SimpleDotComGame.java
@@ -6,29 +6,27 @@ class SimpleDotComGame {
     public static void main(String[] args) {
         int numOfGuesses;
         SimpleDotCom dotCom = new SimpleDotCom();
+        GameHelper helper = new GameHelper();
 
         // Random rand = new Random();
         // int start = rand.nextInt(5);
         int start = (int) (Math.random() * 5); // cast to int
 
-        int count = 0;
-        int[] loc = new int[3];
-        // populate location cells
-        while (count < 3) {
-            // add start to list
-            loc[count] = start;
-            // increment start
-            start++;
-            count++;
-        }
+        int[] locations = {start, start+1, start+2};
         dotCom.setLocationCells(loc);
+        boolean isAlive = true;
 
-        Scanner in = new Scanner(System.in);
-        while (dotCom.numOfHits < dotCom.locationCells.length) {
-            System.out.println("enter a number: ");
-            String currentGuess = in.nextLine();
-            dotCom.checkYourself(currentGuess);
-
-        }
-    }
+        // Scanner in = new Scanner(System.in);
+        while (isAlive == true) {
+            // System.out.println("enter a number: ");
+            // String currentGuess = in.nextLine();
+            String currentGuess = helper.getUserInput("enter a number: ");
+            String result = dotCom.checkYourself(currentGuess);
+            numOfGuesses++;
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took " + numOfGuesses + " guesses");
+            } // close if
+        } // close while
+    } // close main
 }
