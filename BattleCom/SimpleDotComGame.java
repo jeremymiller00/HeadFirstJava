ea0f4ba5a564af95311f93c2b10742f27f7fc3b7
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-03-29 10:12:34 -0700

    clean up code comments

--- BattleCom/SimpleDotComGame.java
+++ BattleCom/SimpleDotComGame.java
@@ -4,22 +4,17 @@ import java.util.Scanner;
 class SimpleDotComGame {
 
     public static void main(String[] args) {
-        int numOfGuesses;
+        int numOfGuesses = 0;
         SimpleDotCom dotCom = new SimpleDotCom();
         GameHelper helper = new GameHelper();
 
-        // Random rand = new Random();
-        // int start = rand.nextInt(5);
         int start = (int) (Math.random() * 5); // cast to int
 
         int[] locations = {start, start+1, start+2};
-        dotCom.setLocationCells(loc);
+        dotCom.setLocationCells(locations);
         boolean isAlive = true;
 
-        // Scanner in = new Scanner(System.in);
         while (isAlive == true) {
-            // System.out.println("enter a number: ");
-            // String currentGuess = in.nextLine();
             String currentGuess = helper.getUserInput("enter a number: ");
             String result = dotCom.checkYourself(currentGuess);
             numOfGuesses++;
