c1fb71ae6aeb7572bb9a7ebde4e66a08891c2491
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-04-03 11:48:49 -0700

    finish first version game helper

--- BattleCom/GameHelper.java
+++ BattleCom/GameHelper.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
 
+import com.sun.openpisces.AlphaConsumer;
+
 public class GameHelper {
 
     public static final String alphabet = "abcdefg";
@@ -22,6 +24,63 @@ public class GameHelper {
         } catch (IOException e) {
             System.out.println("IOException: " + e);
         }
-        return inputLine;
+        return inputLine.toLowerCase();
+    }
+
+    public ArrayList<String> placeDotCom(int comSize) {
+        ArrayList<String> alpaCells = new ArrayList<String>();
+        String[] alphacoords = new String [comSize]; // holds coordinates (i.e. 'f6')
+        String temp = null;                         // temporary string for concat
+        int[] coords = new int[comSize];            // current candidate coords
+        int attempts = 0;                           // current attempts counter
+        boolean success = false;                    // flag = found a good location?
+        int location = 0;                           // current starting location
+
+        comCount++;                                 // nth dot com to place
+        int incr = 1;                               // set horizontal increment
+        if ((comCount % 2) == 1) {                  // if odd dot com (place horizontaly)
+            incr = gridLength;                      // set vertical increment
+        }
+        
+        while ( !success & attempts++ < 200 ) {
+            location = (int) (Math.random() * gridSize);
+            // System.out.println(" try " + location);
+            int x =0;
+            success = true;
+            while (success && x < comSize) {
+                if (grid[location] == 0) {
+                    coords[x++] = location;
+                    location += incr;
+                    if (location >= gridSize) {
+                        success = false;
+                    }
+                    if (x>0 && (location % gridLength == 0)) {
+                        success = false;
+                    }
+                } else {
+                    // System.out.println(" used " + location)
+                    success = false;
+                }
+            }
+        }
+
+        int x = 0;
+        int row = 0;
+        int column = 0;
+        // System.out.println("\n")
+        while (x < comSize) {
+            grid[coords[x]] = 1;
+            row = (int) (coords[x] / gridLength);
+            column = coords[x] % gridLength;
+            temp = String.valueOf(alphabet.charAt(column));
+
+            alphaCells.add(temp.concat(Integer.toString(row)));
+            x++;
+            // System.out.print(" cord " + x + " = " + alphaCells.get(x-1));
+        }
+
+        // System.out.println("\n");
+
+        return alphaCells;
     }
 }
