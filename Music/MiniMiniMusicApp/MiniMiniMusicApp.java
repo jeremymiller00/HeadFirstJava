fc755eafc540ea293cf47100632abe00ca75a36c
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-05 13:02:30 -0700

    finish v1

--- Music/MiniMiniMusicApp/MiniMiniMusicApp.java
+++ Music/MiniMiniMusicApp/MiniMiniMusicApp.java
@@ -13,7 +13,29 @@ public class MiniMiniMusicApp {
             player.open();
 
             // create a sequence
-            Sequence seq = new Sequence(Sequence.PPQ, 4)
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+
+            // get a track from the sequence
+            Track track = seq.createTrack();
+
+            // put some midi events into the track
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144, 1, 44, 100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
+            track.add(noteOn);
+
+            ShortMessage b = new ShortMessage();
+            b.setMessage(128, 1, 44, 100);
+            MidiEvent noteOff = new MidiEvent(b, 16);
+            track.add(noteOff);
+
+            // give the sequence to the sequencer
+            player.setSequence(seq);
+
+            // push play
+            player.start();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
