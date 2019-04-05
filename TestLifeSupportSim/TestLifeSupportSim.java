4d0019e60fa29d412c62885c926bfdcab10afe23
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-04-05 09:36:23 -0700

    add simunit class

--- TestLifeSupportSim/TestLifeSupportSim.java
+++ TestLifeSupportSim/TestLifeSupportSim.java
@@ -23,3 +23,17 @@ class RetentionBot {
   }
 }
 
+class SimUnit {
+  String botType;
+  SimUnit(String type) {
+    botType = type;
+  }
+  int powerUse() {
+    if ("Retention".equals(botType)) {
+      return 2;
+    } else {
+      return 4;
+    }
+  }
+}
+
