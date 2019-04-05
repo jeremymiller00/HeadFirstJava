1287cbe2c2d9c080e423787428869e9f7f68bd6b
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-05 12:56:18 -0700

    start play method

--- Music/MiniMiniMusicApp/MiniMiniMusicApp.java
+++ Music/MiniMiniMusicApp/MiniMiniMusicApp.java
@@ -1,5 +1,19 @@
 import javax.sound.midi.*;
 
 public class MiniMiniMusicApp {
-    
+    public static void main(String[] args) {
+        MiniMiniMusicApp mini = new MiniMiniMusicApp();
+        mini.play();
+    }
+
+    public void play() {
+        try {
+            // get a sequencer and open it
+            Sequencer player = MidiSystem.getSequencer();
+            player.open();
+
+            // create a sequence
+            Sequence seq = new Sequence(Sequence.PPQ, 4)
+        }
+    }
 }
