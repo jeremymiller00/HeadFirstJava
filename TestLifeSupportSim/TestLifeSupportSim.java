51115989ae218095307d12fd52355e86860a0e27
Jeremy Miller <jeremymiller@SFOLMBP-JMILLER.local>
2019-04-05 09:33:52 -0700

    add V3 class

--- TestLifeSupportSim/TestLifeSupportSim.java
+++ TestLifeSupportSim/TestLifeSupportSim.java
@@ -8,5 +8,13 @@ class V2Radiator {
   }
 }
 
+class V3Radiator extends V2Radiator {
+  V3Radiator(ArrayList lglist) {
+    super(lglist);
+    for (int g=0; g<10; g++) {
+      lglist.add(new SimUnit("Radiator"));
+    }
+  }
+}
 
 
