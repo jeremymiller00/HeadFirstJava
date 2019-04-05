f1256424bfca4c7b372a4df8bc476f37c694eb1f
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-05 13:33:46 -0700

    main method

--- Music/MiniMiniMusicApp/MiniMusicCmdLine.java
+++ Music/MiniMiniMusicApp/MiniMusicCmdLine.java
@@ -1,5 +1,15 @@
 import javax.sound.midi.*;
 
 public class MiniMusicCmdLine {
+    public static void main(String[] Args) {
+        MiniMusicCmdLine mini = new MiniMusicCmdLine();
+        if (args.length < 2) {
+            System.out.println("Don't forget the instrument and note args");
+        } else {
+            int instrument = Integer.parseInt(args[0]);
+            int note = Integer.parseInt(args[1]);
+            mini.play(instrument, note);
+        }
+    }
     
 }
