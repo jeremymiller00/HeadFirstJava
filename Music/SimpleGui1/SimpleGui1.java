473466f2b0420a9e9be80717fd2c27f81a09d961
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-08 09:53:07 -0700

    first version

--- Music/SimpleGui1/SimpleGui1.java
+++ Music/SimpleGui1/SimpleGui1.java
@@ -1,2 +1,19 @@
 import javax.swing.*;
 
+public class SimpleGui1 {
+    public static void main(String[] args) {
+        // make a frame
+        JFrame frame = new Jrame();
+        // make a button, pass text to constructor
+        JButton button = new JButton("click me");
+
+        // exit program when window is closed, or it will just run forever
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // add the button to the frame's content
+        frame.getContentPane().add(button);
+        // set frame size in pixels
+        frame.setSize(300,300);
+        // make frame visible (this is required)
+        frame.setVisible(true);
+    }
+}
