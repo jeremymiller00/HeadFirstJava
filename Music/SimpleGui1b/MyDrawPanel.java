c6795ce415d59c1e3fcec167d5857428cdf15c24
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-09 10:04:14 -0700

    finish version 1

--- Music/SimpleGui1b/MyDrawPanel.java
+++ Music/SimpleGui1b/MyDrawPanel.java
@@ -1,3 +1,17 @@
 import java.awt.*;
 import java.swing.*;
 
+/**
+ * A subclass of JPanel (a widget that you can add to a frame)
+ */
+class MyDrawPanel extends JPanel {
+    // the big important Graphics method that I will never actually call myself
+    // creates a surface of types Graphics for drawing on
+    public void paintComponents(Graphics g) {
+
+        // set the color to orange
+        g.setColor(Color.orange);
+        // define coordinates to fill
+        g.fillRect(20,50,100,100);
+    }
+}
