b8a0d00893ed067abdb60b96cc72891a00b898af
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-12 14:33:13 -0700

    version 2

--- Music/SimpleGui1b/MyDrawPanel.java
+++ Music/SimpleGui1b/MyDrawPanel.java
@@ -1,5 +1,7 @@
 import java.awt.*;
-import java.swing.*;
+import javax.swing.*;
+
+import javax.swing.ImageIcon;
 
 /**
  * A subclass of JPanel (a widget that you can add to a frame)
@@ -7,11 +9,29 @@ import java.swing.*;
 class MyDrawPanel extends JPanel {
     // the big important Graphics method that I will never actually call myself
     // creates a surface of types Graphics for drawing on
-    public void paintComponents(Graphics g) {
+    // public void paintComponent(Graphics g) {
 
-        // set the color to orange
-        g.setColor(Color.orange);
-        // define coordinates to fill
-        g.fillRect(20,50,100,100);
+    //     // set the color to orange
+    //     g.setColor(Color.orange);
+    //     // define coordinates to fill
+    //     g.fillRect(20,50,100,100);
+    // }
+
+    // public void paintComponent(Graphics g) {
+    //     Image image = new ImageIcon("image.jpg")/getImage();
+    //     // image, x nd y coords of top left corner
+    //     g.drawImage(image, 3, 4, this);
+    // }
+
+    public void paintComponent(Graphics g) {
+        // cast to Graphics2D object to make Graphics2D methods available
+        Graphics2D g2d = (Graphics2D) g;
+        GradientPaint gradient = new GradientPaint(70, 70, Color.blue, 150,150, Color.orange);
+
+        // set virtual paintbrush to gradient, rather than solid color
+        g2d.setPaint(gradient);
+        g2d.fillOval(70,70,100,100);
+
     }
+
 }
