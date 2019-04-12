11f0824399377a8d8bb484ae05f32304127026b4
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-12 14:50:28 -0700

    initial commit

---
+++ Music/TwoButtons/TwoButtons.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TwoButtons {
+
+    JFrame frame;
+    JLabel label;
+
+    public static void main(String[] args) {
+        
+    }
+}
