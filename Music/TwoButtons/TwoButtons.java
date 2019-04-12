8c42713087deaae19d70eba900beddedaf6093e7
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-12 14:58:14 -0700

    finish v1

--- Music/TwoButtons/TwoButtons.java
+++ Music/TwoButtons/TwoButtons.java
@@ -19,7 +19,19 @@ public class TwoButtons {
         JButton labelButton = new JButton("Change label");
         labelButton.addActionListener(new LabelListener());
 
+        JButton colorButton = new JButton("Change circle");
+        colorButton.addActionListener(new ColorListener());
 
+        label = new JLabel("I'm a label");
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+        frame.getContentPane().add(BorderLayout.EAST, labelButton);
+        frame.getContentPane().add(BorderLayout.WEST, label);
+
+        frame.setSize(300,300);
+        frame.setVisible(true);
     }
 
     class LabelListener implements ActionListener {
