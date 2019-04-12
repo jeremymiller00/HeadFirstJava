bd3af264ea93df183d43584bd8313a38dd3fb679
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-12 14:55:06 -0700

    add inner classes

--- Music/TwoButtons/TwoButtons.java
+++ Music/TwoButtons/TwoButtons.java
@@ -8,6 +8,30 @@ public class TwoButtons {
     JLabel label;
 
     public static void main(String[] args) {
-        
+        TwoButtons gui = new TwoButtons();
+        gui.go();        
     }
+
+    public void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton labelButton = new JButton("Change label");
+        labelButton.addActionListener(new LabelListener());
+
+
+    }
+
+    class LabelListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            label.setText("Ouch!");
+        }
+    }
+
+    class ColorListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            frame.repaint();
+        }
+    }
+
 }
