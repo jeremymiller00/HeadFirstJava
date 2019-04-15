7dc8d40ac5325ab75d037bb710cc43e70bee02d6
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-15 10:55:35 -0700

    finish v1

--- Music/MiniMusicPlayer2/MiniMusicPlayer2.java
+++ Music/MiniMusicPlayer2/MiniMusicPlayer2.java
@@ -8,6 +8,9 @@ public class MiniMusicPlayer2 implements ControllerEventListener {
         mini.go();
     }
 
+    /** 
+     * 
+     */
     public void go() {
         try {
             // get and open a sequencer
@@ -25,21 +28,40 @@ public class MiniMusicPlayer2 implements ControllerEventListener {
             Track track = seq.createTrack();
 
             for (int i=5; i<60;i+=4) {
-                track.add(makeEvent(144, 1, i, 100, i);
-            }
+                track.add(makeEvent(144, 1, i, 100, i));
+
+                // 176 = ControllerEvent, with argument for event number 127
+                // This even does NOTHING. Just produces an event every time a note is played
+                // Creates somthing that fires that we can listen for
+                // Fires at the same time as NOTE ON
+                track.add(makeEvent(176, 1, 127, 100, i));
+
+                track.add(makeEvent(128, 1, i, 100, i+2));
+            } // and for loop
+
+            sequencer.setSequence(seq);
+            sequencer.setTempoInBPM(220);
+            sequencer.start();
 
         } catch (Exception e) {e.printStackTrace();}
+    } // close go method
+
+    /** 
+     * Event handler method from the ControllerListener interface.
+     * Each time we get an event, print "la" to the command line
+     * @param event - ShortMessage
+     */
+    public void controlChange(ShortMessage event) {
+        System.out.println("la");
     }
 
-
-    
     /**
      * A helper method to simplify the making of several events
      * @param comd - the midi command number to execute
      * @param chan - the channel (instrument) on which to execute the command
-     * @param one - 
-     * @param two
-     * @param tick
+     * @param one - first data byte
+     * @param two - second data byte
+     * @param tick - timestamp
      * @return
      */
     public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
@@ -52,5 +74,4 @@ public class MiniMusicPlayer2 implements ControllerEventListener {
         return event
     } // close makeEvent method
 
-
-}
+} // close class
