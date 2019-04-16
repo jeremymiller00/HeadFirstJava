a8b466bf14cef04894a38fe95f1ce532213bb052
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-16 12:06:00 -0700

    finish read file

--- ReadFile/ReadFile.java
+++ ReadFile/ReadFile.java
@@ -1,2 +1,23 @@
 import java.io.*;
 
+class ReadFile {
+    public static void main(String[] args) {
+
+        try {
+            File myFile = new File("MyFile.txt");
+            FileReader fileReader = new FileReader(myFile);
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String line = null;
+
+            while ( (line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            reader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
