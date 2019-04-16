90a070e499d8269567f3cf9b127ea4ecd402bf8f
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-16 10:46:31 -0700

    change class name to avoid naming conflict with java io class

---
+++ FileWriter/WriteFile.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class FileWriter {
+
+    public static void main(String[] args) {
+
+        try {
+            
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+}
