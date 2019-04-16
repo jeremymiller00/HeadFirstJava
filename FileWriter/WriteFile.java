48c897df7b229f25f75f5ae135ac1b96d807eef0
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-16 10:49:01 -0700

    finish writefile

--- FileWriter/WriteFile.java
+++ FileWriter/WriteFile.java
@@ -1,11 +1,15 @@
 import java.io.*;
 
-class FileWriter {
+class WriteFile {
 
     public static void main(String[] args) {
 
         try {
-            
+            FileWriter writer = new FileWriter("data/file.txt");
+
+            writer.write("this is some data to write\n");
+
+            writer.close();
 
         } catch (IOException e) {
             e.printStackTrace();
