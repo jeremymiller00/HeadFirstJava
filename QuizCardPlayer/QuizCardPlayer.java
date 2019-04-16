2b25127e0824fa144d43a4abdb20262858113996
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-16 13:46:57 -0700

    finish v1

--- QuizCardPlayer/QuizCardPlayer.java
+++ QuizCardPlayer/QuizCardPlayer.java
@@ -1,14 +1,19 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JTextArea;
+
+import com.sun.webkit.ContextMenu.ShowContext;
 
 public class QuizCardPlayer {
     
     private JTextArea display;
     private JTextArea answer;
-    private ArrayList<QuizCard> cardlist;
+    private ArrayList<QuizCard> cardList;
     private int currentCardIndex;
     private JFrame frame;
     private JButton nextButton;
@@ -19,6 +24,9 @@ public class QuizCardPlayer {
         reader.go()
     }
 
+    /**
+     * Build and display gui
+     */
     public void go() {
         // build and display gui
         frame = new JFrame("Quiz Card Player");
@@ -51,11 +59,26 @@ public class QuizCardPlayer {
         frame.setVisible(true);
     } // close go
 
-    class NextCardListener implements ActionListener {
+    /**
+     * Listens for the next card. Show question or answer.
+     */
+    class NextCardListener implements ActionListener { // inner class
         public void actionPerformed(ActionEvent ev) {
-            // if this is a question, show the answer
-            // otherwise show next question
-            // set a flag for whether we're viewing a question or answer
+            if (isShowAnswer) {
+                // show the answer because they've seen the question
+                display.setText(currentCard.getAnswer());
+                nextButton.setText("Next Card");
+                isShowAnswer = false;
+            } else {
+                // show the question
+                if (currentCardIndex < cardList.size()) {
+                    showNextCard();
+                } else {
+                    // there are no more cards
+                    display.setText("No more cards");
+                    nextButton.setEnabled(false);
+                }
+            }
         }
     }
 
@@ -63,6 +86,9 @@ public class QuizCardPlayer {
         public void actionPerformed(ActionEvent ev) {
             // bring up a file dialog box
             // let the user navigate to and choose a card set to open
+            JFileChooser fileOpen = new JFileChooser();
+            fileOpen.showOpenDialog(frame);
+            loadFile(fileOpen.getSelectedFile());
         }
     }
 
@@ -72,6 +98,20 @@ public class QuizCardPlayer {
         // reads the file one line at a time
         // tells the makeCard() method to make a new card out of the new line
         // (one line in the file holds bothe the question and the answer, sep = "/")
+        cardlist = new ArrayList<QuizCard>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ( (line = reader.readLine()) !=null) {
+                makeCard(line);
+            }
+            reader.close();
+
+        } catch (Exception e) {
+            System.out.println("couldn't read the card file");
+            e.printStackTrace();
+        }
+        showNextCard();
     }
 
     private void makeCard(String lineToParse) {
@@ -79,5 +119,17 @@ public class QuizCardPlayer {
         // and parses into two pieces: queston and answer
         // creates a new QuizCard
         // adds it to the ArrayList called CardList
+        String[] result = lineToParse.split("/");
+        QuizCard card = new QuizCard(result[0], result[1]);
+        cardList.add(card);
+        System.out.println("made a card");
     }
-}
+
+    private void showNextCard() {
+        currentCard = cardList.get(currentCardIndex);
+        currentCardIndex++;
+        display.setText(currentCard.getQuestion());
+        nextButton.setText("Show Answer");
+        isShowAnswer = true;
+    }
+} // close class
