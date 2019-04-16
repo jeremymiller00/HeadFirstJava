98cd6b0a8c0b0a9b22accb5da5a3c28e35353219
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-16 11:55:07 -0700

    finish v1

--- QuizCardPlayer/QuizCardBuilder.java
+++ QuizCardPlayer/QuizCardBuilder.java
@@ -1,14 +1,21 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.IOError;
+import java.io.IOException;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
+
+import sun.java2d.pipe.BufferedBufImgOps;
 
 public class QuizCardBuilder {
 
@@ -29,43 +36,45 @@ public class QuizCardBuilder {
         frame = new JFrame("Quiz Card Builder");
         JPanel mainPanel = new JPanel();
         Font bigFont = new Font("sanserif", Font.BOLD, 24);
+
+        // set up area for question
         question = new JTextArea(6, 20);
         question.setLineWrap(true);
         question.setWrapStyleWord(true);
         question.setFont(bigFont);
-
         JScrollPane qScroller = new JScrollPane(question);
         qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
+        //. set up area for answer
         answer = new JTextArea(6, 20);
         answer.setLineWrap(true);
         answer.setWrapStyleWord(true);
         answer.setFont(bigFont);
-
         JScrollPane aScroller = new JScrollPane(answer);
         aScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         aScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
+        // create 'Next Card' button
         JButton nextButton = new JButton("Next Card");
 
         cardList = new ArrayList<QuizCard>();
 
-        JLabel qLabel = new JLabnel("Question");
-        JLabel aLabel = new JLabnel("Answer");
-
+        JLabel qLabel = new JLabel("Question");
+        JLabel aLabel = new JLabel("Answer");
         mainPanel.add(qLabel);
         mainPanel.add(qScroller);
         mainPanel.add(aLabel);
         mainPanel.add(aScroller);
         mainPanel.add(nextButton);
         nextButton.addActionListener(new NextCardListener());
+
+        // create a menu bar
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
         JMenuItem newMenuItem = new JMenuItem("New");
         JMenuItem saveMenuItem = new JMenuItem("Save");
         newMenuItem.addActionListener(new NewMenuListener());
-
         saveMenuItem.addActionListener(new SaveMenuListener());
         fileMenu.add(newMenuItem);
         fileMenu.add(saveMenuItem);
@@ -78,36 +87,50 @@ public class QuizCardBuilder {
 
     private class NextClassListener implements ActionListener { // inner class
         public void actionPerformed(ActionEvent ev) {
-            // add the current card to the list and clear the text areas
-            // triggered when user hits 'Next Card' button
-            // store the card in the list and start a new card
+            QuizCard card = new QuizCard(question.getText(), answer.getText());
+            cardList.add(card);
+            clearCard();
         }
     }
 
     private class SaveMenuListener implements ActionListener { // inner class
         public void actionPerformed(ActionEvent ev) {
-            // bring up a file dialog box
-            // let the user name and save the set
-            // trigered when the user clicks 'save' from the file menu
-            // means user wants to save all current cards in the list to a 'set'
+            QuizCard card = new QuizCard(question.getText(), answer.getText());
+            cardList.add(card);
+
+            // Brings up a dialog box and waits on this line until user chooses 'Save' from the dialog box
+            // FileChooser makes this easier
+            JFileChooser fileSave = new JFileChooser();
+            fileSave.showSaveDialog(frame);
+            saveFrame(fileSave.getSelectedFile());
         }
     }
 
     private class NewMenuListener implements ActionListener { // inner class
         public void actionPerformed(ActionEvent ev) {
             QuizCard card = new QuizCard();
-
-            // clear out the card list, clear out the text areas
-            // trigered when user clicks 'New' from the file menu
-            // means start a brand new set
-            // so we clear out the card list and text areas
         }
     }
 
+    private void clearCard() {
+        question.setText("");
+        answer.setText("");
+        question.requestFocus();
+    }
+
     private void saveFile(File file) {
-        // iterate thro0ugh the list of cards and write each one out to a text file
-        // in a parseable way (in other words, with clear separation between parts)
-        // called by the saeveMenu listener, does the actual file writing
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+            for (QuizCard card:cardList) {
+                writer.write(card.getQuestion() + "/");
+                writer.write(card.getAnswer() + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("couldn't write out card list");
+            e.printStackTrace();
+        }
     }
 
 } // close class 
