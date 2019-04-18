26f9c67256030b8ad3bdbdf21b70e2e034d3a046
Jeremy Miller <jeremymiller00@gmail.com>
2019-04-18 10:06:33 -0700

    finish v1

--- TestMap/TestMap.java
+++ TestMap/TestMap.java
@@ -1,2 +1,17 @@
 import java.util.*;
 
+public class TestMap {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> scores = new HashMap<String, Integer>();
+
+        scores.put("Sarah", 99);
+        scores.put("David", 88);
+        scores.put("Pat", 77);
+
+        System.out.println(scores);
+        System.out.println(scores.get("Pat"));
+    
+    }
+}
